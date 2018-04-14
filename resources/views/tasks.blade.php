5f4c7dc24322ada6c980fe8a57b2711b9402697e
cpotron <35893631+cpotron@users.noreply.github.com>
2018-04-14 20:05:40 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -47,7 +47,7 @@
                             </div>
                         </div>
 
-                        <!-- Add Task Button -->
+                        <!-- Bouton pour s'inscrire -->
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-success">
